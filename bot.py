63148782ad4ec427a92b1037dbfd7acd65700902
MrShandy <mrshandy@yandex.ru>
2021-03-03 17:12:37 +0300

    Добавил команду с информацией

--- bot.py
+++ bot.py
@@ -21,10 +21,23 @@ def get_prefix(bot, message):
         row = cur.fetchone()
         return row
 
-bot: commands.Bot = commands.Bot(command_prefix = get_prefix, instintents = discord.Intents.all())
+bot: commands.Bot = commands.Bot(command_prefix = get_prefix, intents = discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print(f'Авторизован под {bot.user}')
+
+@bot.command()
+async def info(ctx):
+    await ctx.send('Test!')
+    embed = discord.Embed(title='Simple Voice',
+                          description='Создание собственных каналов для каждого участника. Редактирование параметров канала через команды и многое другое.',
+                          url='https://github.com/Shandeika/simple-voice', colour=0x8691c8)
+    embed.set_thumbnail(url='https://photo.shandy-dev.ru/shandy/uploads/ab1d22e5013aef1fb490a5f1e80c977a.png')
+    embed.set_image(url='https://photo.shandy-dev.ru/shandy/uploads/3ce580d24d84b65768beac66bbf12d92.png')
+    embed.set_author(name='Shandy', url='https://github.com/Shandeika/',
+                     icon_url='https://photo.shandy-dev.ru/shandy/uploads/9de56bb9dc3276a0b7cf678809097521.png')
+    embed.set_footer(text='Copyright © 2019–2021 Shandy developer agency All Rights Reserved. © 2021')
+    await ctx.send(embed=embed)    
 
 bot.run(config["Config"]["token"])
